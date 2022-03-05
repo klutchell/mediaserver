ad6295e332809da1ddce093e292ff2e72625dac6
Kyle Harding <kmharding@pm.me>
2022-03-05 20:34:43 +0000

    Pin to arch-specific digests when running update
    Use docker-compose.yml as a personalized environment file and merge
    settings from remaining files.
    
    Change-type: major

--- update.sh
+++ update.sh
@@ -2,40 +2,30 @@
 
 set -euo pipefail
 
-compose_file="${1:-"docker-compose.yml"}"
+compose_file="docker-compose.yml"
+override_file="docker-compose.override.yml"
 
 yq() {
     docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
 }
 
-# shellcheck source=/dev/null
-source registry.sh
+skopeo() {
+    docker run --rm alexeiled/skopeo:latest sh -c "skopeo $*"
+}
+
+echo "---" > "${override_file}"
 
 for image in $(yq eval '.services[].image' "${compose_file}")
 do
     service="$(yq eval ".services[].image | select(. == \"${image}\") | path | .[-2]" "${compose_file}")"
-    image="${image%%:*}"
 
-    case "${image}" in 
-        linuxserver/*) regexp="^(v)?[0-9]+\.[0-9]+(\.[0-9]+)?.*ls[0-9]+$" ;;
-        *) regexp="^(v)?[0-9]+\.[0-9]+\.[0-9]+.*$" ;;
-    esac
+    echo "inspecting $image..."
+    digest=$(skopeo "inspect docker://docker.io/$image" | jq '.Digest' | tr -d '"' )
 
-    for alias in "latest" "stable"
-    do
-        echo "Searching for aliases of ${image}:${alias}..."
-        semver="$(get_aliases "${image}" "${alias}" "${regexp}" | sort -V | tail -n1)" || true
+    pinned_image="${image%%:*}@${digest}"
 
-        [ -n "${semver}" ] && break
-
-        echo "No aliases match filter: ${regexp}"
-        get_aliases "${image}" "${alias}"
-    done
-
-    [ -n "${semver}" ] || continue
-
-    echo "Saving as ${image}:${semver}..."
-    yq eval -i ".services[\"${service}\"].image = \"${image}:${semver}\"" "${compose_file}"
+    echo "recording ${pinned_image}..."
+    yq eval -i ".services.${service}.image=\"${pinned_image}\"" ${override_file}
 done
 
 docker-compose up -d
